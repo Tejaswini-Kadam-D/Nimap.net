db91b5bb433de2944cbc417107d3e0e6b8508fb5
Tejaswini-Kadam-D <kadam.tejaswini912@gmail.com>
2025-02-24 17:13:19 +0530

    Add files via upload

---
+++ CategoryController.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NimapProjectUsingADO.net.Models;
+
+namespace NimapProjectUsingADO.net.Controllers
+{
+    public class CategoryController : Controller
+    {
+        //private readonly IConfiguration configuration;
+        //private CategoryyCrud db;
+
+        private readonly ICategoryyService db;
+
+        public CategoryController(ICategoryyService db)
+        {
+            this.db=db;
+        }
+        public ActionResult Index(int pg)
+        {
+            var m = db.GetCategory();
+           
+            const int pagesize = 10;
+            if (pg < 1)
+            {
+                pg = 1;
+            }
+
+            int recscount = m.Count();
+
+            var pager = new Pager(recscount, pg, pagesize);
+
+            int recskip = (pg - 1) * pagesize;
+
+            var data = m.Skip(recskip).Take(pager.PageSize).ToList();
+
+            this.ViewBag.Pager = pager;
+            return View(m);
+        
+       
+        }
+
+       
+        public ActionResult Details(int id)
+        {
+            var t =db.GetCategoryyById(id);
+            return View(t);
+        }
+
+      
+       
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categoryy c)
+        {
+            try
+            {
+                int result = db.AddCategory(c);
+                if (result >= 1)
+                    return RedirectToAction(nameof(Index));
+                else
+                    TempData["message"] = "not able to add";
+                return View();
+            }
+            catch(Exception ex)
+            {
+                TempData["exp"] = ex.Message;
+                return View();
+            }
+        }
+
+       
+        public ActionResult Edit(int id)
+        {
+            var v = db.GetCategoryyById(id);
+            return View(v);
+        }
+
+      
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categoryy c)
+        {
+            try
+            {
+                int result = db.UpdtateCategory(c);
+                if(result>=1)
+                return RedirectToAction(nameof(Index));
+                else
+                    TempData["message"] = "not able to add";
+                return View();
+
+            }
+            catch(Exception ex)
+            {
+                TempData["exp"] = ex.Message;
+                return View();
+            }
+        }
+
+
+        public ActionResult Delete(int id)
+        {
+            var d = db.DeleteCategory(id);
+            if(d==null)
+            {
+                return NotFound();
+            }
+            return View(d);
+        }
+
+      
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public ActionResult Deletec(int id)
+        {
+            try
+            {
+                int result = db.DeleteCategory(id);
+                if(result>=1)
+                return RedirectToAction(nameof(Index));
+                else
+                TempData["message"] = "not able to add";
+                return View();
+
+            }
+            catch (Exception ex)
+            {
+                TempData["exp"] = ex.Message;
+                return View();
+            }
+        }
+    }
+}
---
+++ ProductController.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NimapProjectUsingADO.net.Models;
+
+namespace NimapProjectUsingADO.net.Controllers
+{
+   
+        public class ProductController : Controller
+        {
+            private readonly IConfiguration configuration;
+            private ProducttCrud pcrud;
+            private CategoryyCrud ccrud;
+
+            public ProductController(IConfiguration configuration)
+            {
+                this.configuration = configuration;
+                pcrud = new ProducttCrud(this.configuration);
+                ccrud = new CategoryyCrud(this.configuration);
+            }
+            public IActionResult Index(int pg)
+            {
+                var products = pcrud.GetProducts();
+                const int pagesize = 10;
+                if (pg < 1)
+                {
+                    pg = 1;
+                }
+                int recscount = products.Count();
+                var pager = new Pager(recscount, pg, pagesize);
+                int recskip = (pg - 1) * pagesize;
+                var data = products.Skip(recskip).Take(pager.PageSize).ToList();
+                this.ViewBag.Pager = pager;
+                return View(data);
+                var product = pcrud.GetProducts();
+                return View(product);
+            }
+
+
+            public IActionResult Details(int id)
+            {
+                var product = pcrud.GetProductById(id);
+                return View(product);
+            }
+            [HttpGet]
+            public IActionResult Create()
+            {
+            //ViewBag.clist = ccrud.GetCategory();
+            //return View();
+            var cat = ccrud.GetCategory();
+            ViewData["CategoryId"]=new SelectList(cat,"CategoryId","CategoryName");
+            return View(cat);
+            }
+            [HttpPost]
+            public IActionResult Create(Productt p)
+            {
+                try
+                {
+                    int result = pcrud.AddProduct(p);
+                    if (result >= 1)
+                        return RedirectToAction(nameof(Index));
+                    else
+                    {
+                        TempData["message"] = "Not Able to Add Data";
+                        return View();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    TempData["exp"] = ex.Message;
+                    return View();
+                }
+
+            }
+            public IActionResult Edit(int id)
+            {
+                var p = pcrud.GetProductById(id);
+                ViewBag.clist = ccrud.GetCategory();
+                return View(p);
+
+            }
+            [HttpPost]
+            public IActionResult Edit(Productt p)
+            {
+                int result = 0;
+                result = pcrud.UpdtateProduct(p);
+                if (result >= 1)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["message"] = "Product Not Update";
+                    return View();
+                }
+
+            }
+
+            [HttpGet]
+            public IActionResult Delete(int id)
+            {
+                var p = pcrud.GetProductById(id);
+                return View(p);
+
+            }
+
+            [HttpPost]
+            [ActionName("Delete")]
+            public IActionResult DeleteConfirm(int id)
+            {
+                try
+                {
+                    int result = pcrud.DeleteProduct(id);
+                    if (result >= 1)
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                    else
+                    {
+                        TempData["message"] = "Not Able to Update Data";
+                        return View();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    TempData["exp"] = ex.Message;
+                    return View();
+                }
+
+            }
+        }
+    }
+   
